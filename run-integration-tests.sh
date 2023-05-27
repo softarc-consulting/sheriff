26f4c9cee488e0d7ca5cb0a4e8f26b2cc47d8e1f
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-27 14:36:38 +0200

    feat: add failing sheriff config

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,4 +1,11 @@
 cd test-projects
 cd angular-i
 yarn
-npx ng lint
+cp dynamic-import-sheriff.config.ts sheriff.config.ts
+npx ng lint --output-file dynamic-import-lint.txt
+cmp dynamic-import-lint.txt expected-dynamic-import-lint.txt
+if [ $? -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
