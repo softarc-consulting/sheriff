c886fe69a0332d7745c8507638db254ca13325b9
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-27 20:44:13 +0200

    feat: switch to json format

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -2,8 +2,8 @@ cd test-projects
 cd angular-i
 yarn
 cp dynamic-import-sheriff.config.ts sheriff.config.ts
-npx ng lint --output-file dynamic-import-lint.txt
-cmp dynamic-import-lint.txt expected-dynamic-import-lint.txt
+npx ng lint --format json --output-file dynamic-import-lint.json
+cmp dynamic-import-lint.json expected-dynamic-import-lint.json
 if [ $? -eq 0 ]; then
     exit 0
 else
