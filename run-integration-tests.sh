ba1d2856cc06c29f2ec8a1678a4f2a7d632ecbdf
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-06-06 22:06:54 +0200

    feat: change from cmp to diff

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -4,9 +4,11 @@ yarn
 cp dynamic-import-sheriff.config.ts sheriff.config.ts
 npx ng lint --format json --output-file dynamic-import-lint.json
 cat dynamic-import-lint.json
-cmp dynamic-import-lint.json expected-dynamic-import-lint.json
-if [ $? -eq 0 ]; then
-    exit 0
+
+diff -w dynamic-import-lint.json expected-dynamic-import-lint.json >/dev/null;REPLY=$?
+if [ ${REPLY} -eq 0 ]
+then
+  exit 0
 else
-    exit 1
+  exit 1
 fi
