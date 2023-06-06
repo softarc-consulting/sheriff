a654ec81a384cb3455a7654b1d5b02ce8b64ad2a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-06-06 23:08:19 +0200

    feat: show diff

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -4,7 +4,7 @@ yarn
 cp dynamic-import-sheriff.config.ts sheriff.config.ts
 npx ng lint --format json --output-file dynamic-import-lint.json
 cat dynamic-import-lint.json
-
+diff -w dynamic-import-lint.json expected-dynamic-import-lint.json
 diff -w dynamic-import-lint.json expected-dynamic-import-lint.json >/dev/null;REPLY=$?
 if [ ${REPLY} -eq 0 ]
 then
