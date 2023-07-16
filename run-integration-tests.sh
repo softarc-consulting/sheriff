d45129d52bb110ffee3d9a723724dab5095b38bd
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-07-16 09:48:06 +0200

    fix: use right config file

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,6 +1,6 @@
 cd test-projects
 cd angular-i
 yarn
-cp dynamic-import-sheriff.config.ts sheriff.config.ts
+cp ./tests/dynamic-import-sheriff.config.ts sheriff.config.ts
 npx ng lint --format json --output-file dynamic-import-lint.json
 node tests/verify-linter.js
