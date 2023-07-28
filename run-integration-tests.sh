b63cd2b6634045abc209676080c857fe0536c882
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-07-28 11:13:24 +0200

    fix: integration tests

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,3 +1,5 @@
+set -e
+
 cd test-projects
 cd angular-i
 yarn
@@ -5,17 +7,20 @@ yarn
 # Dynamic Import Check
 echo 'checking for dynamic import error'
 cp ./tests/dynamic-import-sheriff.config.ts sheriff.config.ts
-npx ng lint --format json --output-file dynamic-import-lint.json
+npx ng lint --force --format json --output-file dynamic-import-lint.json
 npx ts-node --esm tests/verify-linter.mts expected-dynamic-import-lint.json dynamic-import-lint.json
 git checkout sheriff.config.ts
 
 ## Deep Import Check
 echo 'checking for deep import error'
 cp ./tests/customers-container.deep-import.component.ts src/app/customers/feature/components/customers-container.component.ts
-npx ng lint --format json --output-file deep-import-lint.json
+npx ng lint --force --format json --output-file deep-import-lint.json
 npx ts-node --esm tests/verify-linter.mts expected-deep-import-lint.json deep-import-lint.json
 git checkout src/app/customers/feature/components/customers-container.component.ts
 
-
 ## Dependency Rule Check
-#echo 'checking for dependency rule error'
+echo 'checking for dependency rule error'
+cp ./tests/customer.dependency-rule.component.ts src/app/customers/ui/customer/customer.component.ts
+npx ng lint --force --format json --output-file dependency-rule-lint.json
+npx ts-node --esm tests/verify-linter.mts expected-dependency-rule-lint.json dependency-rule-lint.json
+git checkout src/app/customers/ui/customer/customer.component.ts
