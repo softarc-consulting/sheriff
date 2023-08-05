d815a218b8473813423512bb866266c476c0b937
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-08-05 20:29:39 +0200

    feat: test typescript version

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,31 +1,9 @@
 set -e
 
-cd test-projects
-cd angular-i
-yarn
+cd test-projects/angular-i
+sh integration-test.sh
 
-# Dynamic Import Check
-echo 'checking for dynamic import error'
-cp ./tests/dynamic-import-sheriff.config.ts sheriff.config.ts
-npx ng lint --force --format json --output-file dynamic-import-lint.json
-npx ts-node --esm tests/verify-linter.mts expected-dynamic-import-lint.json dynamic-import-lint.json
-git checkout sheriff.config.ts
-
-## Deep Import Check
-echo 'checking for deep import error'
-cp ./tests/customers-container.deep-import.component.ts src/app/customers/feature/components/customers-container.component.ts
-npx ng lint --force --format json --output-file deep-import-lint.json
-npx ts-node --esm tests/verify-linter.mts expected-deep-import-lint.json deep-import-lint.json
-git checkout src/app/customers/feature/components/customers-container.component.ts
-
-## Dependency Rule Check
-echo 'checking for dependency rule error'
-cp ./tests/customer.dependency-rule.component.ts src/app/customers/ui/customer/customer.component.ts
-npx ng lint --force --format json --output-file dependency-rule-lint.json
-npx ts-node --esm tests/verify-linter.mts expected-dependency-rule-lint.json dependency-rule-lint.json
-git checkout src/app/customers/ui/customer/customer.component.ts
-
-cd ../..
-
-
+cd ..
+cd typescript-i
+sh ./integration-test.sh
 
--- test-projects/typescript-i/integration-test.sh
+++ test-projects/typescript-i/integration-test.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
 ## TypeScript Versions
 echo 'checking against different TypeScript versions'
 cp -r ../../node_modules/@softarc node_modules
-#npm install typescript@4.8
-npx eslint src --force --format json --output-file linter.json
-npx ts-node --esm ../verify-linter.mts expected-linter.json linter.json
+
+declare -a versions=('4.8' '4.9' '5.0' '5.1')
+
+for version in ${versions[*]}; do
+  npm install typescript@$version
+  installed_version=$(npx tsc -v)
+
+  if [[ ! $installed_version == "Version $version"* ]]
+  then
+    echo "TypeScript should be $version but was $installed_version"
+    exit 1;
+  fi
+
+  npx eslint src --format json --output-file lint.json
+  npx ts-node --esm ../verify-linter.mts ./typescript-i/tests/expected-lint.json ./typescript-i/lint.json
+done
