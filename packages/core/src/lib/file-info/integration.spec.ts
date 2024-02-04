8163bbf20dd0ac467021273acb1a894afa616c3d
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-02-04 22:36:00 +0100

    fix: tests

--- packages/core/src/lib/file-info/integration.spec.ts
+++ packages/core/src/lib/file-info/integration.spec.ts
@@ -2,7 +2,8 @@ import { describe, it } from 'vitest';
 import * as path from 'path';
 import { generateFileInfo } from './generate-file-info';
 import { toFsPath } from './fs-path';
-import { init } from '../init/init';
+import { init } from '../main/init';
+
 
 describe('integration test', () => {
   it.each(['angular-i', 'angular-ii'])('should test $s', (project) => {
@@ -13,7 +14,7 @@ describe('integration test', () => {
       project,
       'src/main.ts'
     );
-    const { tsData } = init(toFsPath(angularMain1), false);
+    const { tsData } = init(toFsPath(angularMain1),  {traverse: true});
     generateFileInfo(toFsPath(angularMain1), false, tsData);
   });
 });
