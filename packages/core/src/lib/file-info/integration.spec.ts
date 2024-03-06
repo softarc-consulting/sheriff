208f594920409de9f13ca1d50e97e713987a987a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-03-06 16:50:40 +0100

    feat: add feature

--- packages/core/src/lib/file-info/integration.spec.ts
+++ packages/core/src/lib/file-info/integration.spec.ts
@@ -5,17 +5,15 @@ import { toFsPath } from './fs-path';
 import { init } from '../main/init';
 
 describe('integration test', () => {
-  for (const project of ['angular-i', 'angular-ii']) {
-    it(`should test ${project}`, () => {
-      const angularMain1 = path.join(
-        __dirname,
-        '../../../../..',
-        'test-projects/',
-        project,
-        'src/main.ts',
-      );
-      const { tsData } = init(toFsPath(angularMain1), { traverse: true });
-      generateUnassignedFileInfo(toFsPath(angularMain1), false, tsData);
-    });
-  }
+  it(`should test full-nx`, () => {
+    const angularMain1 = path.join(
+      __dirname,
+      '../../../../..',
+      'test-projects/',
+      'full-nx',
+      'apps/eternal/src/main.ts',
+    );
+    const { tsData } = init(toFsPath(angularMain1), { traverse: true });
+    generateUnassignedFileInfo(toFsPath(angularMain1), false, tsData);
+  });
 });
