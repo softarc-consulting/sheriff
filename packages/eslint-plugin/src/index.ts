7377760d6b7a44a5221d88c85caac2e3168019c9
sts <sts@guidnew.com>
2024-05-29 19:20:20 -0500

    feat: add support for flat config

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,11 +1,29 @@
-import configs from './lib/config';
-import { deepImport } from './lib/rules/deep-import';
-import { dependencyRule } from './lib/rules/dependency-rule';
+import type { TSESLint } from '@typescript-eslint/utils';
+import { parser } from 'typescript-eslint';
+import legacyAll from './lib/configs/legacy-all.json';
+import tsAllConfig from './lib/configs/ts-all';
+import rules from './lib/rules';
 
-export = {
-  rules: {
-    'deep-import': deepImport,
-    'dependency-rule': dependencyRule,
+const tsPluginBase = {
+  configs: {
+    all: legacyAll,
   },
-  configs,
+  rules,
 };
+
+const tsPlugin: TSESLint.FlatConfig.Plugin = tsPluginBase as Omit<
+  typeof tsPluginBase,
+  'configs'
+>;
+
+const configs = {
+  legacy: legacyAll,
+  flat: tsAllConfig(tsPlugin, parser),
+};
+
+export default {
+  configs,
+  rules,
+  tsPlugin,
+};
+export { configs, rules, tsPlugin };
