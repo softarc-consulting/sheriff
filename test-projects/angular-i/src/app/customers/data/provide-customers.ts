5becad84f8ba2fbc16fc78b5176817bcab65b336
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-05-31 15:53:22 +0300

    test(core): add json imports in integration tests

--- test-projects/angular-i/src/app/customers/data/provide-customers.ts
+++ test-projects/angular-i/src/app/customers/data/provide-customers.ts
@@ -2,6 +2,7 @@ import { provideState } from '@ngrx/store';
 import { customersFeature } from './customers.reducer';
 import { provideEffects } from '@ngrx/effects';
 import { CustomersEffects } from './customers.effects';
+import "./customers.json"
 
 export const provideCustomers = [
   provideState(customersFeature),
