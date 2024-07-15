9f77f8cdaaec24cef6d0d5733afe9d4e2445db31
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-07-15 12:11:37 +0800

    ci: run sheriff.full-spec.ts only in the CI (#104)
    This particular test required that all packages 
    have been built.
    
    Since the pre-push commit should run as fast
    as possible, the full-spec test is not excluded
    and only runs in the CI automatically.

--- packages/core/src/sheriff.full-spec.ts
+++ packages/core/src/sheriff.full-spec.ts
@@ -1,15 +1,19 @@
+/**
+ * This test is excluded by default and only runs in the CI.
+ * This is because, it requires teh built packages
+ **/
 import { describe, it } from 'vitest';
 import * as path from 'path';
-import { generateUnassignedFileInfo } from './generate-unassigned-file-info';
-import { toFsPath } from './fs-path';
-import { init } from '../main/init';
+import { generateUnassignedFileInfo } from './lib/file-info/generate-unassigned-file-info';
+import { init } from './lib/main/init';
+import { toFsPath } from './lib/file-info/fs-path';
 
 describe('integration test', () => {
   for (const project of ['angular-i', 'angular-ii']) {
     it(`should test ${project}`, () => {
       const angularMain1 = path.join(
         __dirname,
-        '../../../../..',
+        '../../..',
         'test-projects/',
         project,
         'src/main.ts',
