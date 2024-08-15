a5e775ab86e0c9909dfa3664d685baedd8463e24
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-08-15 17:32:41 +0200

    fix: .

--- test-projects/angular-i/integration-test.sh
+++ test-projects/angular-i/integration-test.sh
@@ -2,7 +2,7 @@ set -e
 yarn
 yalc add @softarc/sheriff-core @softarc/eslint-plugin-sheriff
 cd node_modules/.bin # yalc doesn't create symlink in node_modules/.bin
-ln -s ./@softarc/sheriff-core/src/bin/main.js ./sheriff
+ln -s ../@softarc/sheriff-core/src/bin/main.js ./sheriff
 cd ../../
 
 # CLI List Check
