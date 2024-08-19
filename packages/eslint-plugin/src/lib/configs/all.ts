ff2ab4fe6c1b43a6cfbdf6d754f40cdf66521691
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-08-19 14:17:33 +0200

    fix(eslint): remove parser from eslint plugin
    The parser is not necessary in a plugin, since
    users should use a valid ESLint config which
    already contains the parser.
    
    Source: https://github.com/typescript-eslint/typescript-eslint/issues/9814#issuecomment-2294946153

--- packages/eslint-plugin/src/lib/configs/all.ts
+++ packages/eslint-plugin/src/lib/configs/all.ts
@@ -1,10 +1,8 @@
 import rules from '../rules';
-import { parser } from 'typescript-eslint';
 import type { TSESLint } from '@typescript-eslint/utils';
 
 export const all: TSESLint.FlatConfig.Config = {
   languageOptions: {
-    parser,
     sourceType: 'module',
   },
   plugins: {
