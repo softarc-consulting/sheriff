01930e0b66ed7d0a0600d3990db880393cc205c8
Tom White <tomwhite007@gmail.com>
2025-04-13 21:11:40 +0100

    feat: programmatic filter to createRule

--- packages/eslint-plugin/src/lib/rules/deep-import.ts
+++ packages/eslint-plugin/src/lib/rules/deep-import.ts
@@ -1,8 +1,10 @@
 import { violatesEncapsulationRule } from '@softarc/sheriff-core';
 import { createRule } from './utils/create-rule';
+import { excludeSheriffConfig } from './utils/file-filter';
 
 export const deepImport = createRule(
   'Deep Import',
+  excludeSheriffConfig,
   (context, node, isFirstRun, filename, sourceCode) => {
     const importValue = (node.source as { value: string }).value;
     const message = violatesEncapsulationRule(
--- packages/eslint-plugin/src/lib/rules/encapsulation.ts
+++ packages/eslint-plugin/src/lib/rules/encapsulation.ts
@@ -1,8 +1,9 @@
 import { violatesEncapsulationRule } from '@softarc/sheriff-core';
 import { createRule } from './utils/create-rule';
-
+import { excludeSheriffConfig } from './utils/file-filter';
 export const encapsulation = createRule(
   'Encapsulation',
+  excludeSheriffConfig,
   (context, node, isFirstRun, filename, sourceCode) => {
     const importValue = (node.source as { value: string }).value;
     const message = violatesEncapsulationRule(
