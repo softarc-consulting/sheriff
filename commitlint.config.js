31d1e2f15319ab36b74086e9e205c0e7c0c86ea7
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-06-16 13:26:31 +0200

    chore: revert non-existing optional scopes
    There is no way to provide optional scopes.
    
    The rule scope-enum only supports `always` or `never`:
    https://commitlint.js.org/reference/rules.html#scope-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [
-      2,
-      'optional',
-      ['core', 'eslint-plugin', 'docs', 'test-projects'],
-    ],
-  },
-};
+module.exports = { extends: ['@commitlint/config-conventional'] };
