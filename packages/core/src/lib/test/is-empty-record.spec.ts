50dce6246c306711d4a13fc26d007a5643a9e65e
Michael Berger <m.gonser@icloud.com>
2025-07-03 07:30:08 +0200

    chore(core): fix spacing

--- packages/core/src/lib/test/is-empty-record.spec.ts
+++ packages/core/src/lib/test/is-empty-record.spec.ts
@@ -5,6 +5,7 @@ describe('isEmptyRecord', () => {
   it('should return false if value is null', () => {
     expect(isEmptyRecord(null)).toBe(false);
   });
+
   it('should return false if value is undefined', () => {
     expect(isEmptyRecord(undefined)).toBe(false);
   });
@@ -12,22 +13,28 @@ describe('isEmptyRecord', () => {
   it('should return false if value is an object with at least one key-value-pair', () => {
     expect(isEmptyRecord({ key: 'value' })).toBe(false);
   });
+
   it('should return false if value is an array', () => {
     expect(isEmptyRecord([])).toBe(false);
   });
+
   it('should return false if value is a string', () => {
     expect(isEmptyRecord('')).toBe(false);
   });
+
   it('should return false if value is a number', () => {
     expect(isEmptyRecord(0)).toBe(false);
   });
+
   it('should return false if value is a boolean', () => {
     expect(isEmptyRecord(true)).toBe(false);
     expect(isEmptyRecord(false)).toBe(false);
   });
+
   it('should return false if value is a function', () => {
     expect(isEmptyRecord(() => {})).toBe(false);
   });
+
   it('should return false for a record with at least one key-value-pair', () => {
     expect(isEmptyRecord<Record<string, string>>({ key: 'value' })).toBe(false);
   });
@@ -35,6 +42,7 @@ describe('isEmptyRecord', () => {
   it('should return true if value is an empty object', () => {
     expect(isEmptyRecord({})).toBe(true);
   });
+
   it('should return true if value is an empty record', () => {
     expect(isEmptyRecord<Record<never, never>>({})).toBe(true);
   });
