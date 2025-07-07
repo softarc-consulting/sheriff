f75fa42fb015d16ab56e797a87d6e0f25123ae8e
Michael Berger <m.gonser@icloud.com>
2025-07-07 06:29:08 +0200

    refactor(core): correct type-narrowing for isEmptyRecord

--- packages/core/src/lib/util/is-empty-record.ts
+++ packages/core/src/lib/util/is-empty-record.ts
@@ -1,9 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export function isEmptyRecord<T>(value: T): value is Record<never, never> {
+export function isEmptyRecord(
+  value: unknown,
+): value is Record<never, never> {
   return (
     value !== null &&
-    value !== undefined &&
     typeof value === 'object' &&
     !Array.isArray(value) &&
     Object.keys(value).length === 0
