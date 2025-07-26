976cf66f5b97fb7aa12ce51117bc02e0b216a9a1
Michael Berger <m.gonser@icloud.com>
2025-07-26 21:29:37 +0200

    feat(core): add JSON Reporter

---
+++ packages/core/src/lib/cli/internal/reporter/json-reporter.ts
@@ -0,0 +1,30 @@
+import { Reporter } from './reporter';
+import { cli } from '../../cli';
+import getFs from '../../../fs/getFs';
+
+import { SheriffViolations } from '../../sheriff-violations';
+
+export class JsonReporter implements Reporter {
+  #options: { outputDir: string; projectName: string };
+  constructor(options: { outputDir: string; projectName: string }) {
+    this.#options = options;
+  }
+
+  createReport(validationResults: SheriffViolations): void {
+    const fs = getFs();
+    const targetPath = fs.join(
+      this.#options.outputDir,
+      this.#options.projectName,
+      'violations' + this.#getReportExtension(),
+    );
+    cli.log(`Creating .json-export`);
+
+    fs.createDir(fs.join(this.#options.outputDir, this.#options.projectName!));
+
+    fs.writeFile(targetPath, JSON.stringify(validationResults, null, 2));
+  }
+
+  #getReportExtension(): string {
+    return '.json';
+  }
+}
