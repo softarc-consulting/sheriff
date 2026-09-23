20390ff3b28100cd2d2f08e34fc3578aff08900c
Aukevanoost <auke.v.o@hotmail.com>
2026-09-23 11:57:18 +0200

    feat: expose raw import specifiers in getProjectData (#266)
    Adds an includeRawImports option that maps each resolved import
    to the specifier it was written with.

--- packages/core/src/lib/api/tests/get-project-data.spec.ts
+++ packages/core/src/lib/api/tests/get-project-data.spec.ts
@@ -33,4 +33,34 @@ describe('getProjectData', () => {
     const projectData = getProjectData('src/main.ts', '/projects');
     expect(projectData).toBeDefined();
   });
+
+  it('should include raw imports', () => {
+    createProject(
+      {
+        'tsconfig.json': tsConfig({ paths: { '@lib/*': ['libs/*'] } }),
+        'sheriff.config.ts': sheriffConfig({ modules: {}, depRules: {} }),
+        'src/main.ts': ['./test.ts', '@lib/kit'],
+        'src/test.ts': [],
+        'libs/kit/index.ts': [],
+      },
+      '/projects',
+    );
+
+    const projectData = getProjectData('src/main.ts', '/projects', {
+      includeRawImports: true,
+    });
+
+    expect(projectData['src/main.ts'].rawImports).toEqual({
+      'src/test.ts': './test.ts',
+      'libs/kit/index.ts': '@lib/kit',
+    });
+    expect(projectData['src/test.ts'].rawImports).toEqual({});
+  });
+
+  it('should not include raw imports by default', () => {
+    setupProject();
+
+    const projectData = getProjectData('src/main.ts', '/projects');
+    expect(projectData['src/main.ts'].rawImports).toBeUndefined();
+  });
 });
